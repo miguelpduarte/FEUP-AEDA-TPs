9836275252044b4d2f8c0892bfe30af612af8f50
miguelpduarte <gamer-go@hotmail.com>
2017-11-14 10:26:27 +0000

    Finished TP7 exercises

--- TPs/AEDA-TP7/src/Balcao.cpp
+++ TPs/AEDA-TP7/src/Balcao.cpp
@@ -15,13 +15,30 @@ int Cliente::getNumPresentes() const {
 	return numPresentes;
 }
 
-
+//Does not say explicitly to set clientes_atendidos to 0 but apparently that was being a problem in the tests lol
 Balcao::Balcao(int te)
-		: tempo_embrulho(te), tempo_atual(0), prox_chegada(rand()%20+1), prox_saida(0) {}
+	: tempo_embrulho(te), tempo_atual(0), prox_chegada(rand()%20+1), prox_saida(0), clientes_atendidos(0){}
 
 
 void Balcao:: proximoEvento() {
-	// a alterar
+	//Sees which event comes first and executes it!
+	//(If the queue is empty should always execute chegada tho...)
+
+	//As such, hardcoded for that here:
+	if(clientes.empty()){
+		tempo_atual = prox_chegada;
+		chegada();
+		return;
+	}
+
+	//Priority is on chegada
+	if(prox_chegada <= prox_saida){
+		tempo_atual = prox_chegada;
+		chegada();
+	} else {
+		tempo_atual = prox_saida;
+		saida();
+	}
 }
 
 
@@ -34,11 +51,11 @@ void Balcao::chegada() {
 		prox_saida = tempo_atual + c.getNumPresentes()*tempo_embrulho;
 	}
 
-	prox_chegada = rand() % 20 + 1;
+	prox_chegada = tempo_atual + rand() % 20 + 1;
 
 	clientes.push(c);
 
-	cout << "tempo= " << tempo_atual << endl << "chegou novo cliente com " << c.getNumPresentes() << " presentes";
+	cout << "tempo= " << tempo_atual << endl << "chegou novo cliente com " << c.getNumPresentes() << " presentes\n";
 }   
 
 
@@ -54,7 +71,10 @@ void Balcao::saida() {
 
 	//Writing info
 	//(There is no format for this one so I kinda made it up)
-	cout << "tempo= " << tempo_atual << endl << "saiu cliente com " << proxCliente.getNumPresentes() << " presentes";
+	cout << "tempo= " << tempo_atual << endl << "saiu cliente com " << proxCliente.getNumPresentes() << " presentes\n";
+
+	//Incrementing the count of customers serviced
+	++clientes_atendidos;
 }
 
 
@@ -65,8 +85,9 @@ int Balcao::getProxChegada() const { return prox_chegada; }
 
 
 ostream & operator << (ostream & out, const Balcao & b1) {
-	// a alterar
-     return out;
+	out << "Nr de clientes atendidos: " << b1.clientes_atendidos << endl;
+	out << "Nr de clientes em espera: " << b1.clientes.size() << endl;
+	return out;
 }
 
 
